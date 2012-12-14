52de2f7e8d2b584e99556036316ae59264799ad7
Piotr Adamski <mcveat@gmail.com>
2012-12-14 15:27:40 +0100

    removed wrapping of received document

--- app/controllers/DocumentController.java
+++ app/controllers/DocumentController.java
@@ -13,17 +13,19 @@
 */
 package controllers;
 
-import play.mvc.*;
-
+import play.api.templates.Html;
+import play.mvc.Controller;
+import play.mvc.Result;
 import util.pdf.PDF;
-import views.html.*;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 public class DocumentController extends Controller {
 
 	public static Result document() {
         final String body = request().body().asText();
-        if(body == null || body.isEmpty()) return badRequest("Make sure to provide some content as [text/plain; charset=UTF-8]");
-        return PDF.ok(document.render(body));
+        if(isNullOrEmpty(body)) return badRequest("Make sure to provide some content as [text/plain; charset=UTF-8]");
+        return PDF.ok(new Html(body));
 	}
 
 }
