81440152e23594a3eeb13c635880d9cf47077ef8
Piotr Adamski <mcveat@gmail.com>
2012-12-20 12:43:55 +0100

    local ivy repository resolver

--- project/Build.scala
+++ project/Build.scala
@@ -4,14 +4,17 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "FinPDF"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName = "FinPDF"
+  val appVersion = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "pdf" % "pdf_2.9.1" % "0.3.1"
-    )
+  val appDependencies = Seq(
+    "pdf" % "pdf_2.9.1" % "0.3.1"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    resolvers ++= Seq(
+      Resolver.file("Local Ivy Repository", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
     )
+  )
 
 }
